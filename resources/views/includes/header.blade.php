ad8f70dbb0184d81aa76b95b1434b356936dd150
Dana Meidiana <dana.mediana@gmail.com>
2020-12-22 21:47:26 +0700

    header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,21 +3,7 @@
     <script src="{{ asset('adminlte/bower_components/jquery/dist/jquery.min.js') }}"></script>
     <meta id="meta-csrf" content="{{csrf_token()}}">
 </head>
-<div style="width: auto; height:10vh">
-    <div class="topnav">
-        <a href="/"><img src="theluxegrad.png" style="width:12vw; height:7vh; float:left; margin-right: 50px;"></a>
-        <a href="/">Home</a>
-        <a href="/Login">Login</a>
-        <a href="/Register">Register</a>
-        <a href="/admin">Admin</a>
-        <a href="/profiluser">Profil User</a>
-        <a href="/aboutus">About us</a>
-    </div>
-    <div  style="height: 2px; background-color: yellow; border: none;">
 
-    </div>
- 
-</div>
 
 <body class="hold-transition skin-blue layout-top-nav">
     <!--div style="width: auto; height:10vh"-->
